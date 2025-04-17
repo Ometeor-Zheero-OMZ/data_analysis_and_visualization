cc6ce6ef5fd226bb94141f77fcf9f30cffc93bb8
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2025-04-18 00:12:26 +0900

    memo

---
+++ numpy/universal_array_function/main.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+arr = np.arange(11)
+print(arr)
+
+print(np.sqrt(arr))
+print(np.exp(arr))
+
+a = np.random.randn(10)
+print(a)
+
+b = np.random.randn(10)
+print(b)
+
+print(np.add(a, b))
+print(np.maximum(a, b))
